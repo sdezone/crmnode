0379315d2456eadbc327e5f0c024dfac267c9214
sdezone <mh.nusretkaya@hotmail.com>
2022-07-12 11:24:24 +0300

    default route added.

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
     res.send('Hello World');
 });
 
+app.use((req, res) => {
+    res.status(404).send('404 Not Found');
+});
+
 var server = app.listen(8081, function () {
     var host = server.address().address
     var port = server.address().port
